d5a002646d10cb5c320c65696e9393a6b36e0162
Anshul Sahni <anshul_sahni@live.com>
2015-11-16 00:05:44 +0530

    all games and about structure added

--- js/index.js
+++ js/index.js
@@ -3,16 +3,32 @@
 var disabled=[40,37,38,39];
 impress().init();
 var current_key=0;
+var vertical_nav=0;
 $("body").keydown(function(event){
-	console.log(current_key);
-	if((event.keyCode==65 || event.keyCode==39) && current_key>-2)
-		current_key--;
-	else if((event.keyCode==68 || event.keyCode==37) && current_key<4)
-		current_key++;
+	console.log(event.keyCode);
+	if((event.keyCode==65 || event.keyCode==39) && current_key>-2 && vertical_nav==0)
+	{	current_key--; flag='h';	}
+	else if((event.keyCode==68 || event.keyCode==37) && current_key<4 && vertical_nav==0)
+	{	current_key++; flag='h';	}
+	else if((event.keyCode==83 || event.keyCode==87) && current_key==0)
+	{
+		if(event.keyCode==83 && vertical_nav<1)
+			vertical_nav++;
+		else if(event.keyCode==87 && vertical_nav>0)
+			vertical_nav--;
+		flag='v'
+	}
 	else
 		return;
-
-	nSlide="slide"+(current_key);
+	if(flag=='h')
+		nSlide="slide"+(current_key);
+	else
+	{
+		if(vertical_nav==0)
+			nSlide="slide0";
+		else
+			nSlide="slide0"+vertical_nav;
+	}
 	impress().goto(nSlide);
 	event.preventDefault();
 });
