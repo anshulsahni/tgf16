03f555275fda81c3871d5689f9254560f684182a
Ansh <anshul_sahni@live.com>
2016-01-05 21:19:41 +0530

    mobile development initiated

---
+++ mobile/nav.php
@@ -0,0 +1,10 @@
+<div data-role='header' data-id='nav_bar' data-position="fixed">
+	<div data-role='navbar'>
+		<ul>
+			<li><a href='./index.html' data-transition="slide">Home</a></li>
+			<li><a href="./special.html" data-transition="slide">Special</a></li>
+			<li><a href="./games.html" data-transition="slide">Games</a></li>
+			<li><a href="./contact.html" data-transition="slide">Contacts</a></li>
+		</ul>
+	</div>
+</div>
