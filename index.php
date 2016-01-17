50fc09b05d14cea94669e5fc46b2cb9ca75c63b8
Anshul Sahni <anshul_sahni@live.com>
2016-01-17 19:02:50 +0530

    added automatic redirection to mobile site

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript">
+		var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+			    if (mobile) {
+			       	window.location = "http://m.srmgamingclub.in";
+			    }
+			    else{
+			    	window.location="./index.html"
+			    }
+	</script>	
+</head>
+
+</html>
